221099906deb3deb188dc52ba5faf27f96819176
Nur Hidayat <hidayat365@gmail.com>
2019-10-28 12:02:21 +0700

    upgrade to dotnet core 3.0

--- Accounting/Startup.cs
+++ Accounting/Startup.cs
@@ -33,7 +33,7 @@ namespace Accounting
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddDbContext<AccountingDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("local")));
         }
 
